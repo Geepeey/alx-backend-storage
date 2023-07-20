8d1a6dc1a57f774b8bdb80fad2a87581b4f68a82
Geepeey <harshmallins@gmail.com>
2023-07-20 01:25:35 +0000

    ADT

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,41 +4,35 @@ Implement a get_page function (prototype: def get_page(url: str) -> str:).
 The core of the function is very simple. It uses the requests module to
 obtain the HTML content of a particular URL and returns it.
 """
-
 import redis
 import requests
+from typing import Callable
 from functools import wraps
 
 r = redis.Redis()
 
 
-def url_access_count(method):
-    """decorator for get_page function"""
-    @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
+def count_url_access(func: Callable) -> Callable:
+    """Decorator function that counts the number
+    of times a particular URL was accessed
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """Function responsible for incrementing count"""
+        key = "count:{}".format(*args)
+        text = func(*args, **kwargs)
+        r.incr(key)
         r.expire(key, 10)
-        return html_content
+        return text
     return wrapper
 
 
-@url_access_count
+@count_url_access
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
+    """Obtains the HTML content of a particular
+    URL.
+    Returns:
+        content (str): The HTML content of a url
+    """
     results = requests.get(url)
     return results.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
